eedfe8211254913a953eda30ab77f4af437a8f38
Nikita <32339836+nacenik@users.noreply.github.com>
2020-09-24 08:56:09 +0300

    Added exit command functional

--- src/main/java/net/oleksin/socket/commandfactory/command/ExitCommand.java
+++ src/main/java/net/oleksin/socket/commandfactory/command/ExitCommand.java
@@ -4,6 +4,7 @@ public class ExitCommand implements Command {
 
   @Override
   public void execute(CommandContext commandContext) {
-
+    commandContext.changeConnectionFlag();
+    commandContext.addToOutput("Goodbye Man! See you soon!");
   }
 }
