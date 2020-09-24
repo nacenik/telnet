928fc4f72a047b4fe28259930bc8078aa7cef66f
Nikita <32339836+nacenik@users.noreply.github.com>
2020-09-24 08:56:30 +0300

    Added error command functional

--- src/main/java/net/oleksin/socket/commandfactory/command/ErrorCommand.java
+++ src/main/java/net/oleksin/socket/commandfactory/command/ErrorCommand.java
@@ -4,5 +4,6 @@ public class ErrorCommand implements Command {
 
   @Override
   public void execute(CommandContext commandContext) {
+    commandContext.addToOutput("Error! You had a mistake in command!");
   }
 }
