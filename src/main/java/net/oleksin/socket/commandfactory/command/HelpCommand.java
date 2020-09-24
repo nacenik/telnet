5ccbc694135c4b7986892423a8f69f0030dbd537
Nikita <32339836+nacenik@users.noreply.github.com>
2020-09-24 08:55:51 +0300

    Added help command functional

--- src/main/java/net/oleksin/socket/commandfactory/command/HelpCommand.java
+++ src/main/java/net/oleksin/socket/commandfactory/command/HelpCommand.java
@@ -4,6 +4,12 @@ public class HelpCommand implements Command {
   
   @Override
   public void execute(CommandContext commandContext) {
-
+    commandContext.addToOutput("|||\tHELP\t|||\n"
+            + "cat [filename] | [absolute filepath]\t - \tread file\n"
+            + "cd [] | [..] | [absolute path] | [path]\t -\t change current directory\n"
+            + "echo [string]\t - \treturn string\n"
+            + "exit\t - \tclose connection\n"
+            + "ls\t - \toutput all files in current directory\n"
+            + "stat\t - \toutput information about connection\n");
   }
 }
